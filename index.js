c0d138ddf64fe0482cf00308cf7806131cc138b6
Ali Shalabi <ashalabi13@gmail.com>
2020-05-06 15:01:22 -0700

    Reformatted wrapper to include two helper functions

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const clientID = process.env.CLIENT_ID
 const apiKEY = process.env.API_KEY
 
 const test = new ApiWrapper(apiKEY, "pizza", "94117")
-// test.getNamesAndReviews().then((json) => {
-//   console.log(json)
-// })
-// test.allOpen().then((json) => {
-//   console.log(json)
-// })
-// test.onlyHigherThanCertainStars(4).then((json) => {
-//   console.log(json)
-// })
-// test.onlyCertainPricing(2).then((json) => {
-//   console.log(json)
-// })
+test.getNamesAndReviews().then((json) => {
+  console.log(json)
+})
+test.allOpen().then((json) => {
+  console.log(json)
+})
+test.onlyHigherThanCertainStars(4).then((json) => {
+  console.log(json)
+})
+test.onlyCertainPricing(2).then((json) => {
+  console.log(json)
+})
 test.getRandom().then((json) => {
   console.log(json)
 })
--- yelp-wrapper.js
+++ yelp-wrapper.js
@@ -14,61 +14,65 @@ class ApiWrapper {
     this.client = yelp.client(this.apiKEY)
   }
 
-  async getNamesAndReviews() {
+  // Helper function to return json
+  async connection() {
     const res = await this.client.search(this.searchRequest)
     const result = res.jsonBody.businesses
-    const prettyJson = JSON.stringify(result, null, 4)
-    const ret = result.map(business => {
+    return result
+  }
+
+  // Helper function to format return json
+  formatNameReview(array) {
+    const ret = array.map(business => {
       return {name: business.name, rating: business.rating}
     })
     return ret
   }
 
+  // Get name + reviews based on unfiltered search parameters
+  async getNamesAndReviews() {
+    const result = await this.connection()
+    const ret = this.formatNameReview(result)
+    return ret
+  }
+
+  // Filter: only allow open restaurants
   async allOpen() {
-    const res = await this.client.search(this.searchRequest)
-    const unfiltered = res.jsonBody.businesses
+    const unfiltered = await this.connection()
     const filtered = unfiltered.filter(business => {
       return business.is_closed === false
     })
-    // const prettyJson = JSON.stringify(filtered, null, 4)
-    const ret = filtered.map(business => {
-      return {name: business.name, rating: business.rating, pricing: business.price}
-    })
+    const ret = this.formatNameReview(filtered)
     return ret
   }
 
+  // Filter: rating higher than set amount of stars
+  // Param: star is an int between 0 and 5
   async onlyHigherThanCertainStars(stars) {
-    const res = await this.client.search(this.searchRequest)
-    const unfiltered = res.jsonBody.businesses
+    const unfiltered = await this.connection()
     const filtered = unfiltered.filter(business => {
       return business.rating >= stars
     })
-    // const prettyJson = JSON.stringify(filtered, null, 4)
-    const ret = filtered.map(business => {
-      return {name: business.name, rating: business.rating, pricing: business.price}
-    })
+    const ret = this.formatNameReview(filtered)
     return ret
   }
 
+  // Filter: only price equal to set pricing
+  // Param: pricing is a string between "$" and "$$$$"
   async onlyCertainPricing(pricing) {
-    const res = await this.client.search(this.searchRequest)
-    const unfiltered = res.jsonBody.businesses
+    const unfiltered = await this.connection()
     const filtered = unfiltered.filter(business => {
       return business.price == "$".repeat(pricing)
     })
-    // const prettyJson = JSON.stringify(filtered, null, 4)
-    const ret = filtered.map(business => {
-      return {name: business.name, rating: business.rating, pricing: business.price}
-    })
+    const ret = this.formatNameReview(filtered)
     return ret
   }
 
+  // Get random restautant based on unfiltered search
   async getRandom() {
-    const res = await this.client.search(this.searchRequest)
-    const unfiltered = res.jsonBody.businesses
+    const unfiltered = await this.connection()
     const arrayLen = unfiltered.length
     const randomInt = Math.floor(Math.random() * Math.floor(arrayLen - 1))
-    // console.log(arrayLen)
     const business = unfiltered[randomInt]
     return {name: business.name, rating: business.rating}
   }
@@ -76,7 +80,3 @@ class ApiWrapper {
 }
 
 module.exports = ApiWrapper;
-
-// test = new ApiWrapper(apiKEY, "pastrami", "94117")
-
-// test.getNamesAndReviews()
